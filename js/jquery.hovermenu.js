bb74b928e7d30f77f85c7ad24c96a35913d1116e
Michael Hoy <michael.john.hoy@gmail.com>
2012-12-04 19:32:42 -0500

    initial copy

---
+++ js/jquery.hovermenu.js
@@ -0,0 +1,50 @@
+(function () {
+
+  $.fn.hoverMenu = function () {
+
+    var hideNested = function (nested) {
+      nested.removeClass( 'revealed' );
+      nested.parents( 'li.expanded' ).removeClass( 'child-revealed' );
+    };
+
+    var showNested = function (nested) {
+      nested.addClass( 'revealed' );
+      nested.parents( 'li.expanded' ).addClass( 'child-revealed' );
+    };
+
+    return this.each( function () {
+
+      var top = $( this )
+        , nested = $( 'ul.menu', top );
+
+      $( nested ).each( function () {
+        var child = $( this )
+          , link = child.siblings( 'a' )
+          , n = 0
+          , tid = 0
+          , check = function () { 
+              if ( tid ) clearTimeout( tid ); 
+              setTimeout(function () {
+                if (n < 1) { 
+                  n = 0; 
+                  hideNested( child );
+                }
+                tid = 0;
+              }, 500);
+            };
+
+        link.mouseover( function (e) {
+          hideNested( nested );
+          showNested( child );
+          n++;
+        });
+
+        link.mouseout( function (e) { n--; check(); });
+        child.mouseover( function (e) { n++; });
+        child.mouseout( function (e) { n--; check(); });
+        link.click( function () { return false; });
+        link.addClass( 'unclick' );
+      });
+    });
+  };
+})();
